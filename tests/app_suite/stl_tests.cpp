85cdf37852a00ba3d9a91cb878fed2eb6077873a
Timur Iskhodzhanov <timurrrr@google.com>
2011-06-29 13:09:42 +0000

    Add a couple of ostringstream tests that exhibit the xlocale leak (see issue 257)
    SVN-Revision: 361

---
+++ tests/app_suite/stl_tests.cpp
@@ -0,0 +1,38 @@
+/* **********************************************************
+ * Copyright (c) 2011 Google, Inc.  All rights reserved.
+ * **********************************************************/
+
+/* Dr. Memory: the memory debugger
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; 
+ * version 2.1 of the License, and no later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "gtest/gtest.h"
+
+#include <sstream>
+
+TEST(StlTests, ostringstreamTest) {
+    // This is a known leak in xlocale internals, see
+    // http://code.google.com/p/drmemory/issues/detail?id=257
+    std::ostringstream os;
+    os << "BOO\n" << 1 << "\n";
+}
+
+TEST(StlTests, wostringstreamTest) {
+    // This is a known leak in xlocale internals, see
+    // http://code.google.com/p/drmemory/issues/detail?id=257
+    std::wostringstream wos;
+    wos << L"BOO\n" << 1 << L"\n";
+}
