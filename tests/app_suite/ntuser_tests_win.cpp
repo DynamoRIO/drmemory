599b2ee7c8a39b1b18c1c370dbb988613be81b6a
Timur Iskhodzhanov <timurrrr@google.com>
2011-07-18 10:05:01 +0000

    Add a test for NtUserSystemParametersInfo syscall to app_suite
    SVN-Revision: 398

---
+++ tests/app_suite/ntuser_tests_win.cpp
@@ -0,0 +1,32 @@
+/* **********************************************************
+ * Copyright (c) 2011 Google, Inc.  All rights reserved.
+ * **********************************************************/
+
+/* Dr. Memory: the memory debugger
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; 
+ * version 2.1 of the License, and no later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <windows.h>
+
+#include "gtest/gtest.h"
+
+TEST(NtUserTests, SystemParametersInfo) {
+    NONCLIENTMETRICS metrics;
+    ZeroMemory(&metrics, sizeof(NONCLIENTMETRICS));
+    metrics.cbSize = sizeof(NONCLIENTMETRICS);
+    EXPECT_NE(0, SystemParametersInfo(SPI_GETNONCLIENTMETRICS,
+                                      sizeof(NONCLIENTMETRICS), &metrics, 0));
+}
