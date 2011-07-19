252cf72b002d3c07dc6450e88385e556af3131f5
Timur Iskhodzhanov <timurrrr@google.com>
2011-07-19 15:27:21 +0000

    Add a test given as a reproducer in issue 45 [fixed] to app_suite to make sure it remains w/o reports
    SVN-Revision: 402

--- tests/app_suite/ntuser_tests_win.cpp
+++ tests/app_suite/ntuser_tests_win.cpp
@@ -20,6 +20,7 @@
  */
 
 #include <windows.h>
+#include <winuser.h>
 
 #include "gtest/gtest.h"
 
@@ -29,4 +30,44 @@ TEST(NtUserTests, SystemParametersInfo) {
     metrics.cbSize = sizeof(NONCLIENTMETRICS);
     SystemParametersInfo(SPI_GETNONCLIENTMETRICS, sizeof(NONCLIENTMETRICS),
                          &metrics, 0);
+}
+
+namespace Clipboard_Tests {
+void WriteStringToClipboard(const std::string& str) {
+    HWND hWnd = ::GetDesktopWindow();
+    ASSERT_NE(0, ::OpenClipboard(hWnd));
+    ::EmptyClipboard();
+    HGLOBAL data = ::GlobalAlloc(2 /*GMEM_MOVABLE*/, str.size() + 1);
+    ASSERT_NE((HGLOBAL)NULL, data);
+
+    char* raw_data = (char*)::GlobalLock(data);
+    memcpy(raw_data, str.data(), str.size() * sizeof(char));
+    raw_data[str.size()] = '\0';
+    ::GlobalUnlock(data);
+
+    ASSERT_EQ(data, ::SetClipboardData(CF_TEXT, data));
+    ::CloseClipboard();
+}
+
+void ReadAsciiStringFromClipboard(std::string *result) {
+    assert(result != NULL);
+
+    HWND hWnd = ::GetDesktopWindow();
+    ASSERT_NE(0, ::OpenClipboard(hWnd));
+
+    HANDLE data = ::GetClipboardData(CF_TEXT);
+    ASSERT_NE((HANDLE)NULL, data);
+
+    result->assign((const char*)::GlobalLock(data));
+
+    ::GlobalUnlock(data);
+    ::CloseClipboard();
+}
+
+TEST(NtUserTests, ClipboardPutGet) {
+    std::string tmp, str = "ASCII";
+    WriteStringToClipboard(str);
+    ReadAsciiStringFromClipboard(&tmp);
+    ASSERT_STREQ("ASCII", tmp.c_str());
+}
 }
