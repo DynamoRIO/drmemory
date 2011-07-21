e96ee097c5c09422ee8c3bddd2dffac9df300654
Timur Iskhodzhanov <timurrrr@google.com>
2011-07-21 09:49:13 +0000

    Add CreateTextServices test to app_suite to add issue 501 coverage
    SVN-Revision: 405

---
+++ tests/app_suite/ntgdi_tests_win.cpp
@@ -0,0 +1,33 @@
+/* **********************************************************
+ * Copyright (c) 2011 Google, Inc.  All rights reserved.
+ * **********************************************************/
+
+/* Dr. Memory: the memory debugger
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; 
+ * version 2.1 of the License, and no later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <windows.h>
+
+#include <richedit.h>
+#include <textserv.h>
+
+#include "gtest/gtest.h"
+
+#pragma comment(lib, "riched20.lib")
+
+TEST(NtGdiTests, CreateTextServices) {
+    CreateTextServices(NULL, NULL, NULL);  // it fails but it's OK
+}
