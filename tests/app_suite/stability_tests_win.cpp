17e567c799483ed227796b784db9644cee1c0b48
Timur Iskhodzhanov <timurrrr@google.com>
2011-08-04 09:16:54 +0000

    Add a stability tests used as a repro for i#532 crash with -no_check_uninitialized
    SVN-Revision: 452

---
+++ tests/app_suite/stability_tests_win.cpp
@@ -0,0 +1,30 @@
+/* **********************************************************
+ * Copyright (c) 2011 Google, Inc.  All rights reserved.
+ * **********************************************************/
+
+/* Dr. Memory: the memory debugger
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; 
+ * version 2.1 of the License, and no later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <windows.h>
+#include "gtest/gtest.h"
+
+TEST(StabilityTests, FormatMessage) {
+    // Was: http://code.google.com/p/drmemory/issues/detail?id=532
+    OutputDebugStringA("Hello!");
+    char buff[256];
+    FormatMessageA(0x00001200, NULL, 0, 0, buff, 256, NULL);
+}
