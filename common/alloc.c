c378265946ccd17ef58baf13ad905764c679b679
Derek Bruening <bruening@google.com>
2013-05-14 14:59:10 +0000

    Fixes issue 1229
    Fix _DebugHeapDelete identification bug: do not demand that the call
    to free() be the 2nd or later call, as not all instances of
    _DebugHeapDelete call the destructor.  The check for libc should be
    sufficient to avoid labeling the call to the destructor.
    
    SVN-Revision: 1359

--- common/alloc.c
+++ common/alloc.c
@@ -1426,19 +1426,18 @@ find_debug_delete_interception(app_pc mod_start, app_pc mod_end, size_t modoffs)
                 /* i#1031: we may not have processed libc yet.  Doing so at
                  * process init won't solve potential issues w/ a later-loaded
                  * lib depending on a still-not-yet-processed libc (if app doesn't
-                 * use libc), so we assume that any 2nd call (1st is to destructor)
-                 * to libc is a call to free.  Even if there are 2 destructor call
-                 * instances, no destructor should live in libc.
+                 * use libc), so we assume that any call to libc is a call to
+                 * free.  We can't rely on this being the 2nd call as there's
+                 * not always a call to the destructor.  No destructor should
+                 * live in libc.
                  */
-                (num_calls > 0 && pc_is_in_libc(tgt) && !pc_is_in_libc(mod_start))) {
+                (pc_is_in_libc(tgt) && !pc_is_in_libc(mod_start))) {
                 LOG(2, "%s: found cti to free? "PFX"\n", __FUNCTION__, tgt);
                 modoffs = pc - mod_start;
                 found = true;
                 break;
             }
         }
-        if (instr_is_call(&inst))
-            num_calls++;
         /* Should find before ret and within one page (this is a short routine) */
     } while (!instr_is_return(&inst) && npc - (mod_start + modoffs) < PAGE_SIZE);
     instr_free(drcontext, &inst);
