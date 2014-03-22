58ef289dfef38db203b8fa86a403ec1415e58926
Derek Bruening <bruening@google.com>
2014-03-22 02:11:42 +0000

    Add new test for inserting assembly instruction sequences without having to go and update line numbers every time in registers.res.  This could have gone into the app_suite but we don't have a nice asm build setup there.
    SVN-Revision: 1800

---
+++ tests/asmtest.c
@@ -0,0 +1,89 @@
+/* **********************************************************
+ * Copyright (c) 2014 Google, Inc.  All rights reserved.
+ * **********************************************************/
+
+/* Dr. Memory: the memory debugger
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License, and no later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef ASM_CODE_ONLY /* C code ***********************************************/
+
+/* registers.c is getting large and unwieldy, with every change requiring
+ * line# changes in registers.res.  This is essentially an app_suite asm
+ * test, with no errors expected, making it easier to expand.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void asm_test(char *undef, char *def);
+
+void
+asm_test_C(void)
+{
+    char undef[128];
+    char def[128] = {0,};
+    asm_test(undef, def);
+}
+
+int
+main(int argc, char *argv[])
+{
+    asm_test_C();
+
+    printf("all done\n");
+    return 0;
+}
+
+#else /* asm code *************************************************************/
+#include "cpp2asm_defines.h"
+START_FILE
+
+#define FUNCNAME asm_test
+/* void asm_test(char *undef); */
+        DECLARE_FUNC(FUNCNAME)
+GLOBAL_LABEL(FUNCNAME:)
+        mov      REG_XAX, ARG1
+        mov      REG_XDX, ARG2
+        push     REG_XBP
+        mov      REG_XBP, REG_XSP
+        END_PROLOG
+
+        /* ensure top zeroed bits from shr are defined, and test bswap */
+        mov      ecx, DWORD [REG_XAX] /* undef */
+        shr      ecx, 16
+        bswap    ecx
+        movzx    ecx, cx
+        cmp      ecx, HEX(1)
+
+        movdqu   xmm0, [REG_XAX] /* undef */
+        mov      ecx, DWORD [REG_XAX] /* undef */
+        pxor     xmm1, xmm1
+        punpcklwd xmm0, xmm1
+        pextrw   ecx, xmm0, 7 /* top word came from xmm1 so defined */
+        cmp      ecx, HEX(2)
+
+        /* XXX: add more tests here */
+
+        add      REG_XSP, 0 /* make a legal SEH64 epilog */
+        mov      REG_XSP, REG_XBP
+        pop      REG_XBP
+        ret
+        END_FUNC(FUNCNAME)
+#undef FUNCNAME
+
+END_FILE
+#endif
