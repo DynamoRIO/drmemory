4b156d9b394ee9c44f76bcf30577ec49c712438e
Derek Bruening <bruening@google.com>
2015-08-21 00:25:17 -0400

    i#1669 windows 10: fix vsyscall assert
    Fixes an assert about KUSER_SHARED_DATA due to incorrectly inferring
    that there is a vsyscall page for Windows 10 WOW64 due to the syscall
    wrapper containing a call through edx.
    
    Review-URL: https://codereview.appspot.com/263920043

--- drmemory/syscall_windows.c
+++ drmemory/syscall_windows.c
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2011-2014 Google, Inc.  All rights reserved.
+ * Copyright (c) 2011-2015 Google, Inc.  All rights reserved.
  * Copyright (c) 2007-2010 VMware, Inc.  All rights reserved.
  * **********************************************************/
 
@@ -148,7 +148,9 @@ vsyscall_pc(void *drcontext, byte *entry)
             return NULL;
         }
         if (opc == OP_mov_imm && opnd_is_reg(instr_get_dst(&instr, 0)) &&
-            opnd_get_reg(instr_get_dst(&instr, 0)) == REG_EDX) {
+            opnd_get_reg(instr_get_dst(&instr, 0)) == REG_EDX &&
+            /* win10 wow64 also has call* edx: exclude it */
+            !is_wow64_process()) {
             ASSERT(opnd_is_immed_int(instr_get_src(&instr, 0)), "internal error");
             vpc = (byte *) opnd_get_immed_int(instr_get_src(&instr, 0));
         }
