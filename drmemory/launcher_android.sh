54179231bd528e28186e6f0bf6287a3553527e2c
Derek Bruening <bruening@google.com>
2016-03-05 10:26:38 -0500

    i#1855 scripts: ensure preservation of quoted tokens in Android frontend script
    Switches from $* to "$@" to ensure preservation of quoted tokens in the
    frontend script used on Android.
    
    Fixes #1855
    
    Review-URL: https://codereview.appspot.com/286580043

--- drmemory/launcher_android.sh
+++ drmemory/launcher_android.sh
@@ -1,4 +1,5 @@
 #!/system/bin/sh
 # There's no DT_RPATH support on Android so we have a script set the load path.
 home="${0%/*}/.."
-LD_LIBRARY_PATH=$home/dynamorio/lib32/@build_type@ $home/bin/launcher $*
+# We need $@ to preserve quoted tokens (as opposed to $*)
+LD_LIBRARY_PATH=$home/dynamorio/lib32/@build_type@ $home/bin/launcher "$@"
