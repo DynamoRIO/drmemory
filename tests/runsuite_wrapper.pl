b40ee4690778cdeec69186a7410e412a5efc60a3
Derek Bruening <bruening@google.com>
2017-02-21 17:20:29 -0500

    i#1938: initial AppVeyor config (#1939)
    Adds an initial attempt at an AppVeyor configuration file, based on
    DynamoRIO's.  We do a build-only with VS2010 and full tests with VS2013.
    
    Adds cygwin support to runsuite_wrapper.
    
    Adds support for no CC env var.
    
    Adds BUILD_MINGW option and uses it to disable mingw tests until we figure out why AppVeyor MinGW g++ crashes.
    
    Adds a list of known AppVeyor failing tests to get things green for the new workflow, until we can fix them.

--- tests/runsuite_wrapper.pl
+++ tests/runsuite_wrapper.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 # **********************************************************
-# Copyright (c) 2016 Google, Inc.  All rights reserved.
+# Copyright (c) 2016-2017 Google, Inc.  All rights reserved.
 # **********************************************************
 
 # Dr. Memory: the memory debugger
@@ -33,14 +33,18 @@ use strict;
 use Cwd 'abs_path';
 use File::Basename;
 my $mydir = dirname(abs_path($0));
+my $is_CI = 0;
 
 # Forward args to runsuite.cmake:
 my $args = '';
 for (my $i = 0; $i <= $#ARGV; $i++) {
+    $is_CI = 1 if ($ARGV[$i] eq 'travis');
     if ($i == 0) {
         $args .= ",$ARGV[$i]";
     } else {
-        $args .= "\\;$ARGV[$i]";
+        # We don't use a backslash to escape ; b/c we'll quote below, and
+        # the backslash is problematically converted to / by Cygwin perl.
+        $args .= ";$ARGV[$i]";
     }
 }
 
@@ -60,17 +64,31 @@ if ($child) {
     }
     close(CHILD);
 } else {
-    system("ctest -VV -S ${mydir}/runsuite.cmake${args} 2>&1");
+    if ($^O eq 'cygwin') {
+        # CMake is native Windows so pass it a Windows path.
+        # We use the full path to cygpath as git's cygpath is earlier on
+        # the PATH for AppVeyor and it fails.
+        $mydir = `/usr/bin/cygpath -wi \"$mydir\"`;
+        chomp $mydir;
+    }
+    # To shrink the log sizes and make Travis and Appveyor error pages easier
+    # to work with we omit a second V and instead use --output-on-failure.
+    # We rely on runsuite_common_post.cmake extracting configure and build error
+    # details from the xml files, as they don't show up with one V.
+    system("ctest --output-on-failure -V -S \"${mydir}/runsuite.cmake${args}\" 2>&1");
+    exit 0;
 }
 
 my @lines = split('\n', $res);
 my $should_print = 0;
 my $exit_code = 0;
-foreach my $line (@lines) {
+for (my $i = 0; $i < $#lines; ++$i) {
+    my $line = $lines[$i];
+    my $fail = 0;
+    my $name = '';
     $should_print = 1 if ($line =~ /^RESULTS/);
     if ($line =~ /^([-\w]+):.*\*\*/) {
-        my $fail = 0;
-        my $name = $1;
+        $name = $1;
         if ($line =~ /build errors/ ||
             $line =~ /configure errors/ ||
             $line =~ /tests failed:/) {
@@ -78,12 +96,56 @@ foreach my $line (@lines) {
         } elsif ($line =~ /(\d+) tests failed, of which (\d+)/) {
             $fail = 1 if ($2 < $1);
         }
-        if ($fail) {
-            $exit_code++;
-            print "\n====> FAILURE in $name <====\n";
+    } elsif ($line =~ /^\s*ERROR: diff contains/) {
+        $fail = 1;
+        $should_print = 1;
+        $name = "diff pre-commit checks";
+    }
+    if ($fail && $is_CI && $^O eq 'cygwin' && $line =~ /tests failed/) {
+        # FIXME i#1938: ignoring certain AppVeyor test failures until
+        # we get all tests passing.
+        my $is_32 = $line =~ /-32/;
+        my %ignore_failures_32 = ('procterm' => 1,
+                                  'winthreads' => 1,
+                                  'malloc_callstacks' => 1,
+                                  'app_suite.pattern' => 1,
+                                  'app_suite' => 1,
+                                  'drstrace_unit_tests' => 1);
+        my %ignore_failures_64 = ('handle' => 1,
+                                  'app_suite' => 1,
+                                  'app_suite.pattern' => 1,
+                                  'drstrace_unit_tests' => 1);
+        # Read ahead to examine the test failures:
+        $fail = 0;
+        my $num_ignore = 0;
+        for (my $j = $i+1; $j < $#lines; ++$j) {
+            my $test;
+            if ($lines[$j] =~ /^\t(\S+)\s/) {
+                $test = $1;
+                if (($is_32 && $ignore_failures_32{$test}) ||
+                    (!$is_32 && $ignore_failures_64{$test})) {
+                    $lines[$j] = "\t(ignore: i#1938) " . $lines[$j];
+                    $num_ignore++;
+                } elsif ($test =~ /_FLAKY$/) {
+                    # Don't count toward failure.
+                } else {
+                    $fail = 1;
+                }
+            } else {
+                last if ($lines[$j] =~ /^\S/);
+            }
         }
+        $line =~ s/: \*/, but ignoring $num_ignore for i1938: */;
+    }
+    if ($fail) {
+        $exit_code++;
+        print "\n====> FAILURE in $name <====\n";
     }
     print "$line\n" if ($should_print);
+}
+if (!$should_print) {
+    print "Error: RESULTS line not found\n";
+    $exit_code++;
 }
 
 exit $exit_code;
