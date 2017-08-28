37c19608c6dbac7fd432652c46b62e75637d0329
Toshi Piazza <toshi.piazza@gmail.com>
2017-08-28 15:12:03 -0400

    i#1726: Port Umbra to ARM
    Pulls out shadow_table_insert_app_to_shadow() into 2 arch-specific
    methods for x86 and for ARM. Also implements this method for ARM.
    
    Issue: #1726

--- umbra/umbra_x86.c
+++ umbra/umbra_x86.c
@@ -249,6 +249,13 @@ shadow_table_app_to_shadow(umbra_map_t *map, app_pc app_addr)
             shadow_table_get_block_offset(map, app_addr));
 }
 
+#if defined(X86)
+int
+umbra_num_scratch_regs_for_translation_arch(void)
+{
+    return 1;
+}
+
 /* code sequence:
  * %reg_index   = %reg_addr;
  * %reg_index >>= 16;
@@ -256,15 +263,13 @@ shadow_table_app_to_shadow(umbra_map_t *map, app_pc app_addr)
  * %reg_addr   += table[%reg_index];
  */
 static void
-shadow_table_insert_app_to_shadow(void *drcontext,
-                                  umbra_map_t *map,
-                                  instrlist_t *ilist,
-                                  instr_t *where,
-                                  reg_id_t reg_addr,
-                                  reg_id_t reg_index)
+shadow_table_insert_app_to_shadow_arch(void *drcontext, umbra_map_t *map,
+                                       instrlist_t *ilist, instr_t *where,
+                                       reg_id_t reg_addr, reg_id_t *regs)
 {
-#ifdef X86
     uint disp;
+    reg_id_t reg_index = regs[0];
+
     /* %reg_index = %reg_addr */
     PRE(ilist, where, XINST_CREATE_move(drcontext,
                                         opnd_create_reg(reg_index),
@@ -295,10 +300,84 @@ shadow_table_insert_app_to_shadow(void *drcontext,
                                                              sizeof(ptr_int_t),
                                                              disp,
                                                              OPSZ_PTR)));
-#elif defined(ARM)
-    /* FIXME i#1726: port to ARM by adding more XINST_CREATE_ routines */
-#endif
 }
+#elif defined(ARM)
+int
+umbra_num_scratch_regs_for_translation_arch(void)
+{
+    return 2;
+}
+
+/* code sequence:
+ * %reg_idx   = table
+ * %reg_tmp   = (%reg_addr >> 0x10)
+ * %reg_idx  += (%reg_tmp  << 0x02)
+ * %reg_idx   = 0x0(%reg_idx)
+ *
+ * %reg_addr  >>= map->scale
+ * %reg_addr   += %reg_idx
+ */
+static void
+shadow_table_insert_app_to_shadow_arch(void *drcontext, umbra_map_t *map,
+                                       instrlist_t *ilist, instr_t *where,
+                                       reg_id_t reg_addr, reg_id_t *regs)
+{
+    reg_id_t reg_idx = regs[0];
+    reg_id_t reg_tmp = regs[1];
+
+    uint disp_hi = (uint)(map->shadow_table) & 0xffff0000;
+    uint disp_lo = (uint)(map->shadow_table) & 0x0000ffff;
+    disp_hi >>= 0x10;
+
+    /* %reg_idx = table */
+    PRE(ilist, where, INSTR_CREATE_movw(drcontext,
+                                        opnd_create_reg(reg_idx),
+                                        OPND_CREATE_INT16(disp_lo)));
+    PRE(ilist, where, INSTR_CREATE_movt(drcontext,
+                                        opnd_create_reg(reg_idx),
+                                        OPND_CREATE_INT16(disp_hi)));
+
+    /* %reg_tmp = (%reg_addr >> 0x10) */
+    PRE(ilist, where, INSTR_CREATE_lsr(drcontext,
+                                       opnd_create_reg(reg_tmp ),
+                                       opnd_create_reg(reg_addr),
+                                       OPND_CREATE_INT8(APP_BLOCK_BITS)));
+
+    /* %reg_idx += (%reg_tmp << 0x02) */
+    PRE(ilist, where, INSTR_CREATE_add_shimm(drcontext,
+                                             opnd_create_reg(reg_idx),
+                                             opnd_create_reg(reg_idx),
+                                             opnd_create_reg(reg_tmp),
+                                             OPND_CREATE_INT(DR_SHIFT_LSL),
+                                             OPND_CREATE_INT(2)));
+
+    /* %reg_idx = 0x0(%reg_idx) */
+    PRE(ilist, where, XINST_CREATE_load(drcontext,
+                                        opnd_create_reg(reg_idx),
+                                        OPND_CREATE_MEMPTR(reg_idx, 0)));
+
+    /* %reg_addr >>= map->scale */
+    if (UMBRA_MAP_SCALE_IS_DOWN(map->options.scale)) {
+        PRE(ilist, where, INSTR_CREATE_lsr(drcontext,
+                                           opnd_create_reg(reg_addr),
+                                           opnd_create_reg(reg_addr),
+                                           OPND_CREATE_INT8(map->shift)));
+    } else {
+        DR_ASSERT(UMBRA_MAP_SCALE_IS_UP(map->options.scale));
+        PRE(ilist, where, INSTR_CREATE_lsl(drcontext,
+                                           opnd_create_reg(reg_addr),
+                                           opnd_create_reg(reg_addr),
+                                           OPND_CREATE_INT8(map->shift)));
+    }
+
+    /* %reg_addr += %reg_idx */
+    PRE(ilist, where, XINST_CREATE_add(drcontext,
+                                       opnd_create_reg(reg_addr),
+                                       opnd_create_reg(reg_idx)));
+}
+#else
+# error NYI
+#endif
 
 static bool
 shadow_table_is_in_default_block(umbra_map_t *map, byte *shadow_addr,
@@ -809,12 +888,6 @@ umbra_value_in_shadow_memory_arch(IN    umbra_map_t *map,
     return DRMF_SUCCESS;
 }
 
-int
-umbra_num_scratch_regs_for_translation_arch(void)
-{
-    return 1;
-}
-
 drmf_status_t
 umbra_insert_app_to_shadow_arch(void *drcontext,
                                 umbra_map_t *map,
@@ -824,10 +897,11 @@ umbra_insert_app_to_shadow_arch(void *drcontext,
                                 reg_id_t *scratch_regs,
                                 int num_scratch_regs)
 {
-    if (num_scratch_regs < 1 || scratch_regs == NULL)
+    if (num_scratch_regs < umbra_num_scratch_regs_for_translation_arch() ||
+        scratch_regs == NULL)
         return DRMF_ERROR_INVALID_PARAMETER;
-    shadow_table_insert_app_to_shadow(drcontext, map, ilist, where,
-                                      addr_reg, scratch_regs[0]);
+    shadow_table_insert_app_to_shadow_arch(drcontext, map, ilist, where,
+                                           addr_reg, scratch_regs);
     return DRMF_SUCCESS;
 }
 
