4a261bb4e141ce19fcad081b691523eff1bb43ac
johnfxgalea <john.galea@stx.ox.ac.uk>
2020-06-28 08:45:45 +0100

    Add lines

--- tests/framework/umbra_client_faulty_redzone.c
+++ tests/framework/umbra_client_faulty_redzone.c
@@ -62,6 +62,7 @@ event_signal_instrumentation(void *drcontext, dr_siginfo_t *info);
 static void
 exit_event(void);
 
+
 DR_EXPORT void
 dr_client_main(client_id_t id, int argc, const char *argv[])
 {
