a21c7599a3f331fc65b073f74408184b66a199c4
Derek Bruening <bruening@google.com>
2020-11-27 11:35:04 -0500

    i#2317: Fix shadow_check_range() crash
    Fixes a bug where shadow_check_range() bypasses the shadow layer that
    handles lazily-allocated shadow mappings and thus can crash.
    
    Adds a test that reproduces the bug.
    
    Adds a new DRMEMORY_ANNOTATE_MAKE_UNADDRESSABLE annotation to make it
    easier to write the test.
    
    Updates the allocator to use uint instead of ushort for its header's
    request_diff field, as ushort is too small for a large allocation like
    the one used in the test.  Since that affects the layout, goes ahead
    and updates the prev_size_shr field to uint as well.
    
    Fixes #2317

--- drmemory/shadow.c
+++ drmemory/shadow.c
@@ -1,5 +1,5 @@
 /* **********************************************************
- * Copyright (c) 2010-2019 Google, Inc.  All rights reserved.
+ * Copyright (c) 2010-2021 Google, Inc.  All rights reserved.
  * Copyright (c) 2007-2010 VMware, Inc.  All rights reserved.
  * **********************************************************/
 
@@ -811,7 +811,7 @@ shadow_check_range(app_pc start, size_t size, uint expect,
         } else if (SHADOW_IS_SHARED_ONLY(info.shadow_type)) {
             incr = info.app_base + info.app_size - pc;
         } else {
-            val = bitmapx2_dword((bitmap_t)info.shadow_base, pc-info.app_base);
+            val = shadow_get_byte(&info, pc);
             val = dqword_to_val(val);
             if (val == UINT_MAX) {
                 /* mixed: have to drop to per-byte */
